cf92354046403532909bdb21cf222df7cf5f6b16
Peety <dubman06@gmail.com>
2025-11-11 23:27:23 +0100

    Implement Entities struct with component registration and entity creation methods

--- src/ecs/entities.rs
+++ src/ecs/entities.rs
@@ -5,14 +5,48 @@ use std::{
     rc::Rc,
 };
 
+#[derive(Default)]
 pub struct Entities {
     components: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any>>>>>,
 }
 
+impl Entities {
+    pub fn register_component<T: Any>(&mut self) {
+        self.components.insert(TypeId::of::<T>(), vec![]);
+    }
+
+    pub fn create_entity(&mut self) {
+        self.components
+            .iter_mut()
+            .for_each(|(key, components)| components.push(None));
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct Health(pub u32);
+
+    struct Speed(pub u32);
+
     #[test]
-    fn register_an_entity() {}
+    fn register_component() {
+        let mut entities = Entities::default();
+        entities.register_component::<Health>();
+        let type_id = TypeId::of::<Health>();
+        let health_components = entities.components.get(&type_id).unwrap();
+        assert_eq!(health_components.len(), 0);
+    }
+
+    #[test]
+    fn create_entity() {
+        let mut entities = Entities::default();
+        entities.register_component::<Health>();
+        entities.register_component::<Speed>();
+        entities.create_entity();
+        let health = entities.components.get(&TypeId::of::<Health>()).unwrap();
+        let speed = entities.components.get(&TypeId::of::<Speed>()).unwrap();
+        assert!(health.len() == speed.len() && health.len() == 1);
+        assert!(health[0].is_none() && speed[0].is_none());
+    }
 }
