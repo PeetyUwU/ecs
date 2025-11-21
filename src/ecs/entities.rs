413c4bb1f41f6120d743662e18ec12eb2460b08b
Peety <dubman06@gmail.com>
2025-11-21 13:10:48 +0100

    feat: Add `with_component` method for attaching components to entities, including error handling and method chaining.

--- src/ecs/entities.rs
+++ src/ecs/entities.rs
@@ -5,22 +5,45 @@ use std::{
     rc::Rc,
 };
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct Entities {
     components: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any>>>>>,
 }
 
 impl Entities {
-    pub fn register_component<T: Any>(&mut self) {
-        self.components.insert(TypeId::of::<T>(), vec![]);
+    pub fn register_component<T: Any + 'static>(&mut self) {
+        let type_id = TypeId::of::<T>();
+        self.components.insert(type_id, vec![]);
     }
 
-    pub fn create_entity(&mut self) {
+    pub fn create_entity(&mut self) -> &mut Self {
         self.components
             .iter_mut()
-            .for_each(|(key, components)| components.push(None));
+            .for_each(|(type_id, component)| component.push(None));
+        self
+    }
+
+    pub fn with_component(&mut self, component: impl Any) -> Result<&mut Self, EntitiesError> {
+        let type_id = component.type_id();
+        if let Some(components) = self.components.get_mut(&type_id) {
+            let last_component = components
+                .last_mut()
+                .ok_or(EntitiesError::LastComponentNotFound)?;
+            *last_component = Some(Rc::new(RefCell::new(component)));
+        } else {
+            return Err(EntitiesError::ComponentNotRegistered);
+        }
+        Ok(self)
     }
 }
+
+#[derive(Debug)]
+pub enum EntitiesError {
+    // Component you tried to insert isn't registered
+    ComponentNotRegistered,
+    LastComponentNotFound,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,7 +53,7 @@ mod tests {
     struct Speed(pub u32);
 
     #[test]
-    fn register_component() {
+    fn register_an_entity() {
         let mut entities = Entities::default();
         entities.register_component::<Health>();
         let type_id = TypeId::of::<Health>();
@@ -48,5 +71,25 @@ mod tests {
         let speed = entities.components.get(&TypeId::of::<Speed>()).unwrap();
         assert!(health.len() == speed.len() && health.len() == 1);
         assert!(health[0].is_none() && speed[0].is_none());
+    }
+
+    #[test]
+    fn with_component() {
+        let mut entities = Entities::default();
+        entities.register_component::<Health>();
+        entities.register_component::<Speed>();
+        entities
+            .create_entity()
+            .with_component(Health(100))
+            .unwrap()
+            .with_component(Speed(10))
+            .unwrap();
+
+        let first_health = &entities.components.get(&TypeId::of::<Health>()).unwrap()[0];
+        let wrapped_health = first_health.as_ref().unwrap();
+        let borrowed_health = wrapped_health.borrow();
+        let health = borrowed_health.downcast_ref::<Health>().unwrap();
+
+        assert_eq!(health.0, 100);
     }
 }
